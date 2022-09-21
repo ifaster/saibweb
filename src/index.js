78bac52db62514493451291198b15a680e4097a5
douglasfmSaibWeb <douglas.munaro@saibweb.com.br>
2022-09-21 13:55:53 -0300

    testes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import GlobalStyles from "./styles/GlobalStyles";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
       <GlobalStyles />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
